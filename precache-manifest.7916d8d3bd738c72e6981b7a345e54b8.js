258c02ebfdb3f922c9933d634dfb6e70dc825ecf
Sunnatilla <axakalov@gmail.com>
2019-12-12 11:12:42 +0600

    Updates

---
+++ precache-manifest.7916d8d3bd738c72e6981b7a345e54b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9320250b79d7e7e81ca387d3f17c1d1",
+    "url": "/app-type/index.html"
+  },
+  {
+    "revision": "e200e323a4b29eac7666",
+    "url": "/app-type/static/css/main.63196d51.chunk.css"
+  },
+  {
+    "revision": "cf3a75c6f56b7a97d475",
+    "url": "/app-type/static/js/2.456b0909.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/app-type/static/js/2.456b0909.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e200e323a4b29eac7666",
+    "url": "/app-type/static/js/main.604228b5.chunk.js"
+  },
+  {
+    "revision": "89d96acc3111fcf67149",
+    "url": "/app-type/static/js/runtime-main.1fe2677b.js"
+  }
+]);
